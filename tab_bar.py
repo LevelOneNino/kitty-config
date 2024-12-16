75a71bc187f964523fd06cb7a109d0ef549988b2
Diego Andres Niño Rendon <daninorendon@gmail.com>
2024-12-16 02:30:34 -0500

    kitty config

---
+++ tab_bar.py
@@ -0,0 +1,99 @@
+"""draw kitty tab"""
+# pyright: reportMissingImports=false
+# pylint: disable=E0401,C0116,C0103,W0603,R0913
+
+import datetime
+
+from kitty.fast_data_types import Screen, get_options
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    TabBarData,
+    as_rgb,
+    draw_tab_with_powerline,
+    draw_title,
+)
+from kitty.utils import color_as_int
+
+opts = get_options()
+
+DATE_TIME_FG = as_rgb(color_as_int(opts.color12))
+
+
+def _draw_left_status(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+    use_kitty_render_function: bool = False,
+) -> int:
+    # draw tab title
+    draw_title(draw_data, screen, tab, index)
+
+    extra = screen.cursor.x - before - max_title_length
+
+    if extra > 0:
+        screen.cursor.x -= extra + 1
+        screen.draw("…")
+
+    return screen.cursor.x
+
+
+def _draw_right_status(screen: Screen, is_last: bool) -> int:
+    if not is_last:
+        return screen.cursor.x
+
+
+    cells = [
+        (DATE_TIME_FG, 0, datetime.datetime.now().strftime(" %B %d, %Y | %H:%M ")),
+    ]
+
+    right_status_length = 0
+    for _, _, cell in cells:
+        right_status_length += len(cell)
+
+    draw_spaces = screen.columns - screen.cursor.x - right_status_length
+    if draw_spaces > 0:
+        screen.draw(" " * draw_spaces)
+
+    for fg, bg, cell in cells:
+        screen.cursor.fg = fg
+        screen.cursor.bg = bg
+        screen.draw(cell)
+    screen.cursor.fg = 0
+    screen.cursor.bg = 0
+
+    screen.cursor.x = max(screen.cursor.x, screen.columns - right_status_length)
+    return screen.cursor.x
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    end = _draw_left_status(
+        draw_data,
+        screen,
+        tab,
+        before,
+        max_title_length,
+        index,
+        is_last,
+        extra_data,
+        use_kitty_render_function=False,
+    )
+    _draw_right_status(
+        screen,
+        is_last,
+    )
+    return end
